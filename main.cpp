50808b6f0b2cf16c808b48e5aa8e21aa95492902
Tarcisio Fischer <tarcisio.fischer.cco@gmail.com>
2014-11-23 18:38:37 -0200

    Fixing double argument

--- main.cpp
+++ main.cpp
@@ -8,6 +8,6 @@ T f(const T &x) { return cos(x) * sin(x) + log10(x); }
 int main()
 {
     function_wrapper fw(f<function_t>);
-    std::cout << "h=" << traits::h() << " -> " << derive(fw, 50.f) << "\n"; 
+    std::cout << "h=" << traits::h() << " -> " << derive(fw, 50.) << "\n"; 
 }
 
