c1a2b9a62af30157ae0d461c01e1e1302fcb6025
Tarcisio Fischer <tarcisio.fischer.cco@gmail.com>
2014-11-23 18:36:47 -0200

    Changing float to double and passing as default parameter

--- derive.h
+++ derive.h
@@ -6,22 +6,22 @@
 
 namespace
 {
-    typedef std::complex<float> complex_t;
+    typedef std::complex<double> complex_t;
     typedef std::function<complex_t(const complex_t&)> complex_function;
-    typedef std::conditional<traits::derive_using_complex, complex_t, float>::type function_t;
+    typedef std::conditional<traits::derive_using_complex, complex_t, double>::type function_t;
     typedef std::function<function_t(const function_t&)> function_wrapper;
 }
 
 template<typename T1, typename T2>
-T2 derive(std::function<T1(const T1&)> f, const T2 &x)
+T2 derive(std::function<T1(const T1&)> f, const T2 &x, const T2 &h=traits::h())
 {
-    return (f(x + traits::h()) - f(x)) / traits::h();
+    return (f(x + h) - f(x)) / h;
 }
 
 template<typename T1>
-T1 derive(complex_function f, const T1 &x)
+T1 derive(complex_function f, const T1 &x, const T1 &h=traits::h())
 {
-    auto complex_h = complex_t(.0, traits::h());
+    auto complex_h = complex_t(.0, h);
     auto result = f(x + complex_h) / complex_h;
     return result.real();
 }
